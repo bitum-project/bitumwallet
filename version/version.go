c3be893e04409fb4c67d087ca8a4995caad5c7e0
Bitum Project <50862214+bitum-project@users.noreply.github.com>
2019-07-19 16:47:26 +0500

    Update version.go

--- version/version.go
+++ version/version.go
@@ -19,7 +19,7 @@ const semverAlphabet = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrst
 const (
 	Major = 1
 	Minor = 4
-	Patch = 10
+	Patch = 11
 )
 
 // Integer is an integer encoding of the major.minor.patch version.
